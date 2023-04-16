8b44cdc8e247eafef0d6508ab8f1a18171733b82
tomoki matsubuchi <matsubuchitmk@gmail.com>
2023-04-16 12:54:40 +0900

    Todoを編集できるように変更

--- front/components/TodoList.vue
+++ front/components/TodoList.vue
@@ -1,21 +1,41 @@
 <template>
-  <v-card>
-    <v-card-title>
-      Todo List
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="search"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
-    </v-card-title>
-    <v-data-table :headers="headers" :items="todos" :search="search">
-      <template v-slot:item.action="{ item }">
-        <v-icon small @click="deleteItem(item)">delete</v-icon>
-      </template>
-    </v-data-table>
-  </v-card>
+  <div>
+    <v-card>
+      <v-card-title>
+        Todo List
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table :headers="headers" :items="todos" :search="search">
+        <template v-slot:item.action="{ item }">
+          <v-icon small @click="editItem(item)">edit</v-icon>
+          <v-icon small @click="deleteItem(item)">delete</v-icon>
+        </template>
+      </v-data-table>
+    </v-card>
+
+    <v-dialog v-model="dialog">
+      <v-card>
+        <v-card-title>
+          <h2>TODOタイトル編集</h2>
+        </v-card-title>
+        <p>タイトル</p>
+        <v-text-field v-model="dialogText.title" filled></v-text-field>
+        <p>{{ errorMsg }}</p>
+        <v-btn
+          @click="
+            updateItem(dialogText.id, dialogText.title, dialogText.user_id)
+          "
+          >更新</v-btn
+        >
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
@@ -39,6 +59,10 @@ export default {
           value: "action",
         },
       ],
+      dialog: false,
+      dialogTodo: {},
+      dialogText: "",
+      errorMsg: "",
     };
   },
   computed: {
@@ -47,6 +71,42 @@ export default {
     },
   },
   methods: {
+    async editItem(item) {
+      this.dialog = true;
+      this.dialogTodo = item;
+      this.dialogText = {
+        id: item.id,
+        title: item.title,
+        user_id: item.user,
+      };
+    },
+    async updateItem(id, title, user_id) {
+      if (!title) {
+        return (this.errorMsg = "タイトルが空欄です。");
+      }
+      const data = await axios.patch(`/v1/todos/${id}`, {
+        todo: {
+          title: title,
+          user_id: user_id,
+        },
+      });
+      const updateTodos = this.user.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            title: title,
+          };
+        }
+        return todo;
+      });
+      const updateUser = {
+        ...this.user,
+        todos: updateTodos,
+      };
+      this.$store.commit("auth/setUser", updateUser);
+      this.dialog = false;
+      this.errorMsg = "";
+    },
     async deleteItem(item) {
       const res = confirm("本当に削除しますか?");
       if (res) {
